fbc295264eba8d089795674a578b204bdae0e3b0
werronense <31091965+werronense@users.noreply.github.com>
2020-02-10 13:16:10 -0800

    Update gameController

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-const gameboard = (() => {
-  const board = [[null, null, null], [null, null, null], [null, null, null]];
-
-  const resetBoard = () => {
-    return [[null, null, null], [null, null, null], [null, null, null]];
-  }
-
-  const move = (row, column, player) => {
-    if (!board[row][column]) {
-      board[row][column] = player.mark;
-      console.log(board); // TEMP
-    }
-  }
-
-  return { move };
-})();
-
-
-const gameController = (() => {
-  // resume here
-})();
-
-
 const Player = (name, mark) => {
   return { name, mark };
 }
+
+
+const gameController = (() => {
+  // gameboard module
+  const gameboard = (() => {
+    const resetBoard = () => {
+      return [[null, null, null], [null, null, null], [null, null, null]];
+    }
+
+    const board = resetBoard();
+
+    const move = (row, column, player) => {
+      if (!board[row][column]) {
+        board[row][column] = player.mark;
+        console.log(board); // TEMP
+      }
+    }
+
+    return { move };
+  })();
+
+  const players = [];
+
+  const setPlayers = (name, mark) => {
+    players.push(Player(name, mark));
+    players.push(Player("computer", players[0].mark == "x" ? "o" : "x"));
+  }
+
+  const turn = (row, column) => {
+    gameboard.move(row, column, players[0]);
+    // update this so that computer chooses its own move after player
+    gameboard.move(0, 1, players[1]);
+  }
+
+  return { setPlayers, turn };
+})();
 
 
 function render() {
@@ -32,8 +45,5 @@ function render() {
 
 
 // TEMP:
-const playerOne = Player("playerOne", "x");
-const playerTwo = Player("playerTwo", "o");
-
-gameboard.move(0, 0, playerOne);
-gameboard.move(0, 1, playerTwo);
+gameController.setPlayers("playerOne", "x");
+gameController.turn(0, 2);
